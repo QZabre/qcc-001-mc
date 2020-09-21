4dc3abdf5ed8c748f5bed3a1830c3cb04a0afb11
Mark A. Greenslade <mark@casperlabs.io>
2020-09-21 15:47:47 +0200

    1. Latest state bitmaps.  2. Update state renderer.

--- lib/qz/commands/do_render_status.py
+++ lib/qz/commands/do_render_status.py
@@ -4,17 +4,17 @@ from qz.instrument import INSTRUMENT
 
 # Map: from system state to image file.
 _MAP_STATE_IMG = {
-    # Amp off & temp OK. Apd off.
-    "00": "_01.bmp",
+    # Amp on & temp OK. Apd off.
+    "00": "QCC_00.bmp",
 
-    # Amp off & temp OK. Apd on.
-    "01": "_01.bmp",
+    # Amp on & temp OK. Apd on.
+    "01": "QCC_01.bmp",
 
     # Amp on & temp warning. Apd off.
-    "10": "_02.bmp",
+    "10": "QCC_10.bmp",
 
     # Amp on & temp warning. Apd on.
-    "11": "_02.bmp",
+    "11": "QCC_11.bmp",
 
     # Amp on & temp critical. Apd on.
     # Note - should never be in this state.
@@ -31,16 +31,16 @@ _MAP_STATE_IMG = {
     "31": "_04.bmp",
 
     # Amp off & temp warning. Apd off.
-    "40": "_05.bmp",
+    "40": "QCC_40.bmp",
 
     # Amp off & temp warning. Apd on.
-    "41": "_05.bmp",
+    "41": "QCC_41.bmp",
 
-    # Amp off & temp crticial. Apd off.
-    "50": "_06.bmp",
+    # Amp off & temp critical. Apd off.
+    "50": "QCC_50.bmp",
 
-    # Amp off & temp crticial. Apd on.
-    "51": "_06.bmp",
+    # Amp off & temp critical. Apd on.
+    "51": "QCC_51.bmp",
 }
 
 # Defualt image to display - should never be used.
